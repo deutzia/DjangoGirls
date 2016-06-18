6ad783487db403c168b3288b6b80a6c2f5c4e8b1
deutzia <pinus314@gmail.com>
2016-06-18 17:41:25 +0200

    2

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'miniblog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+	# Examples:
+	# url(r'^$', 'miniblog.views.home', name='home'),
+	# url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'', include('blog.urls')),
 ]
