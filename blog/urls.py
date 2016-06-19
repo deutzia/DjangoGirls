d2a8f6aaa3d6935a7092c9b3970606eeafe07211
deutzia <pinus314@gmail.com>
2016-06-19 12:50:01 +0200

    Things do things. And work.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.new_post, name='new_post'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
